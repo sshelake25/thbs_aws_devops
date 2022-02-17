b06ee64ab07ca34aa34b9eccabd9787d714b3990
Surekha Shelake <shelakesurekha@gmail.com>
2022-02-17 16:30:48 +0530

    added k8s and refer notes

---
+++ AWS/user-data.bash
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# -- update and install docker, git, httpd 
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+
+echo "<h1> Hello from THBS $(hostname -f)<h1>" > /var/www/html/index.html
+
+# http://<public-ip>
+
+http://13.233.155.247  - --- default webpage
